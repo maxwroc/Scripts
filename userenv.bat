602a7fa7a27a43e06407a8581abd0f41815ffcd7
maxwroc <max.chodorowski@gmail.com>
2017-02-24 00:12:41 +0000

    small updates in userenv

--- userenv.bat
+++ userenv.bat
@@ -5,10 +5,11 @@ set scriptsdir=%~dp0
 set programsdir=C:\Programs
 set startdir=D:\Projects
 
-if exist customvars.bat call customvars.bat
+if exist %scriptsdir%customvars.bat call %scriptsdir%customvars.bat
 
 doskey hosts=notepad "C:\Windows\System32\drivers\etc\hosts"
 doskey ls=dir /B
+doskey hlp=doskey /macros:all
 doskey cfg=notepad %scriptsdir%\userenv.bat
 doskey md5=%programsdir%\Checksum\fciv.exe -md5 $1
 doskey ip=for /f "tokens=14" %%a in ('ipconfig ^^^| findstr "IPv4"') do @echo IP: %%a
@@ -21,4 +22,4 @@ chdir /D %startdir%
 echo.
 echo Hello !!! [32mYou're ready to go[0m
 echo.
-echo Type "cfg" to see env options
+echo Type [93mhlp[0m to see env options or [93mcfg[0m to configure them.
