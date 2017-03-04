c87ded682949b7cac280e91a600c6b5cbc071e8f
maxwroc <max.chodorowski@gmail.com>
2017-03-04 21:30:12 +0000

    hlp command returns aliases defined only in our dir; more specific aliases for git

--- batch/git-aliases.bat
+++ batch/git-aliases.bat
@@ -9,7 +9,8 @@ set wrapper=%~dp0git-wrapper.bat
 doskey gts=%wrapper% status $*
 doskey gtcm=git commit -am $*
 doskey gtps=git push -u origin head
-doskey gtpl=git pull origin master
+doskey gtpl=git pull
+doskey gtplm=git pull origin master
 doskey gtb=%wrapper% branch $*
 doskey gtch=%wrapper% checkout $*
 
--- userenv.bat
+++ userenv.bat
@@ -1,4 +1,6 @@
 @echo off
+if "%1" equ "/hlp" goto :hlp
+
 echo Initializing local variables
 
 set scriptsdir=%~dp0
@@ -7,10 +9,12 @@ set startdir=D:\Projects
 
 call %scriptsdir%batch\git-aliases.bat
 
+doskey n=notepad $*
 doskey cdscripts=cd %scriptsdir%
 doskey hosts=notepad "C:\Windows\System32\drivers\etc\hosts"
 doskey ls=dir /B $*
-doskey hlp=doskey /macros:all $*
+doskey hlp=%0 /hlp
+doskey hlpall=doskey /macros:all $*
 doskey cfg=notepad %scriptsdir%\userenv.bat
 doskey md5=%programsdir%\Checksum\fciv.exe -md5 $1
 doskey ip=for /f "tokens=14" %%a in ('ipconfig ^^^| findstr "IPv4"') do @echo IP: %%a
@@ -32,3 +36,37 @@ echo.
 echo Hello !!! [32mYou're ready to go[0m
 echo.
 echo Type [93mhlp[0m to see env options or [93mcfg[0m to configure them.
+
+goto :eof
+
+:hlp
+setlocal enabledelayedexpansion
+set tab=	
+SET "spaces=-------------------------------"
+for /f "tokens=3,* delims=:^=" %%a in ('findstr /s /i doskey %scriptsdir%*.bat') do (
+  set "alias=%%a"
+  ::check if it starts with doskey keyword (skipping code which doesn't set dos keys)
+  if "!alias:~0,6!"=="doskey" (
+    ::removing everything up to and including "doskey" (we could use "~7," instead)
+    set alias=!alias:*doskey=!
+    set "command=%%b"
+    call :formatout
+  )
+)
+
+goto :eof
+
+:formatout
+CALL :padright alias 20
+ECHO  %alias% %command%
+GOTO :eof
+
+:padright
+CALL SET padded=%%%1%% %spaces%
+CALL SET %1=%%padded:~0,%2%%
+GOTO :eof
+
+:padleft
+CALL SET padded=%spaces%%%%1%%
+CALL SET %1=%%padded:~-%2%%
+GOTO :eof
