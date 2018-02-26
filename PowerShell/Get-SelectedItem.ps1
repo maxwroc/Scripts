f0e93718cb11f0458916192ad4ebc62044522c28
Maks <max.chodorowski@gmail.com>
2018-02-26 21:28:48 +0000

    Function for getting item selected by user

---
+++ PowerShell/Get-SelectedItem.ps1
@@ -0,0 +1,83 @@
+
+
+
+
+Function Get-SelectedItem() {
+    Param(
+        [Parameter(Mandatory=$true, Position=1)]
+        [System.Collections.ArrayList]$Items,
+        [Parameter(Mandatory=$false, Position=2)]
+        [string]$Message = $null,
+        [Parameter(Mandatory=$false, Position=3)]
+        [string[]]$PropertiesToDisplay = $null,
+        [Parameter(Mandatory=$false, Position=4)]
+        [switch]$Multiselect = $false
+
+    )
+
+    # If there is only one item then return it
+    if ($Items.Count -eq 1) {
+        return $Items[0]
+    }
+
+    do {
+        if ($Message) {
+            Write-Host $Message
+        }
+
+        if ($PropertiesToDisplay.Count -gt 1) {
+            #[System.Collections.ArrayList]$itemsToDisplay = $Items | select -Property @{n="No"; e={ ($global:count++) }}, * | select -Property $props
+            [System.Collections.ArrayList]$itemsToDisplay = $Items 
+            $itemsToDisplay = $itemsToDisplay | select -Property $PropertiesToDisplay | ForEach-Object {$index=1} {$_; $index++} | select -Property @{ Label="No";Expression={$index} }, *
+            $r = $itemsToDisplay.Add((New-Object PSObject -Property @{No="q"; $($PropertiesToDisplay[0])="to quit"})) 
+            $itemsToDisplay | Format-Table -AutoSize 
+        }
+        else {
+            # Printing list of items
+            $number = 1
+            $Items | ForEach-Object {
+                Write-Host -ForegroundColor Yellow -NoNewline ($number++)
+                $name = $_
+                if ($PropertiesToDisplay) {
+                    $name = $_ | select -ExpandProperty $PropertiesToDisplay[0]
+                }
+
+                Write-Host ". $name"
+            }
+
+            Write-Host -ForegroundColor Yellow -NoNewline "q"
+            Write-Host " to quit"
+        }
+
+        $ans = Read-Host "Please select $(("an item", "one or more (comma-separated) items")[[Bool]$Multiselect])"
+
+        if ($Multiselect -and $ans -like "*,*") {
+            [int]$result = -1
+            $ans = $ans.Split(",") | Where { [int]::TryParse($_, [ref]$result) -and $result -in 1..$($Items.Count) } | select { $_ - 1 } | select -ExpandProperty *
+            $isMultiItemAnswer = $ans.Count -gt 0
+        }
+    }
+    while($ans -ne "q" -and -not $isMultiItemAnswer -and -not ($ans -in 1..$($Items.Count)))
+
+    if ($ans -eq "q") {
+        return $null
+    }
+
+    if ($isMultiItemAnswer) {
+        return $Items | Select -Index $ans
+    }
+
+    return $Items[$ans - 1]
+}
+
+# Collection  of strings
+#Get-SelectedItem (Get-Service | select -first 5 -ExpandProperty Name)
+# Message
+# Get-SelectedItem (Get-Service | select -first 5 -ExpandProperty Name) -Message "Hello chose one of the below"
+# Single item
+#Get-SelectedItem (Get-Service | select -first 1 -ExpandProperty Name)
+# Show property insted of the item
+#Get-SelectedItem (Get-Service | select -first 5) -PropertiesToDisplay Name
+# Multiselect
+#Get-SelectedItem (Get-Service | select -first 5) -PropertiesToDisplay Name -Multiselect
+Get-SelectedItem (Get-Service | select -first 5) -PropertiesToDisplay Name
