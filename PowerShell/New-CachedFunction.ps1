702efa4b1633d11947dddbc1178e754d2ae8c120
maxwroc <max.chodorowski@gmail.com>
2018-03-24 23:41:22 +0000

    Concept for function cache

---
+++ PowerShell/New-CachedFunction.ps1
@@ -0,0 +1,28 @@
+﻿
+$funcCacheDir = ($funcCacheDir, "$($env:TEMP)\PSFuncCache", 1 -ne $null)[0]
+
+Function New-CachedFunction {
+    Param(
+        [string]$Name,
+        [ScriptBlock]$TargetFunction,
+        [string]$KeyName = $null
+    )
+
+    {
+        $TargetFunction
+    }.GetNewClosure()
+}
+
+function Subject {
+    $ans = Read-Host "Enter some val"
+    return @{"hello" = "val $ans"}
+}
+
+${function:Cached-Subject} = New-CachedFunction "Subject" ${function:Subject}
+
+$funcCacheDir
+
+Cached-Subject 1
+#Cached-Subject 1
+#Cached-Subject 2
+#Cached-Subject 1
