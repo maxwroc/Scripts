e6afae8d739c32f5c027719a47c4833463e52e05
maxwroc <max.chodorowski@gmail.com>
2018-07-07 08:14:23 +0100

    Remove redundant comments

--- PowerShell/Restart-WiFiAdapter.ps1
+++ PowerShell/Restart-WiFiAdapter.ps1
@@ -140,8 +140,6 @@ if ($UnregisterLogonTask) {
     exit
 }
 
-# Start-Sleep -s 15
-
 $isInternetConnectionRunning = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}')).IsConnectedToInternet
 
 if (-not $isInternetConnectionRunning) {
